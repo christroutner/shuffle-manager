705d7c793180a851180e4e17b2b4cfac2ac2eb70
Chris Troutner <chris.troutner@gmail.com>
2020-02-02 22:01:52 -0800

    Got prototype fully functional

--- shuffle-manager.js
+++ shuffle-manager.js
@@ -17,6 +17,28 @@ async function checkWallet() {
   try {
     const data = await electronCash.getSmallUtxos()
     console.log(`data: ${JSON.stringify(data,null,2)}`)
+
+    // For each utxo found under the threshold, send it to the CashFusion wallet.
+    // for(let i=0; i < data.length; i++) {
+    for(let i=0; i < 1; i++) {
+      const thisUtxo = data[i]
+
+      const toAddr = await electronCash.getFusionAddr()
+      console.log(`fusion addr: ${toAddr}`)
+
+      const fromAddr = thisUtxo.address
+      console.log(`Sending UTXOs at this address: ${fromAddr}`)
+
+      const hex = await electronCash.sendUtxo(toAddr, fromAddr)
+      // console.log(`hex: ${hex}`)
+
+      const txid = await electronCash.broadcast(hex)
+      console.log(`Transaction broadcast with TXID: ${txid}`)
+
+      console.log(`Sent utxo ${i}`)
+      console.log(' ')
+    }
+
   } catch(err) {
     console.log(`Error in checkWallet(): `, err)
   }
