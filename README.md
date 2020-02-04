8d723ef54d24414a643807436c218741e2c30c3b
Chris Troutner <chris.troutner@gmail.com>
2020-02-03 19:46:43 -0800

    Updating readme

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 This is a daemon app for managing an Electron Cash wallet that uses CashShuffle. The app periodically checks the UTXOs in the wallet. Any that are less than a threshold get sent to a separate CashFusion wallet for consolidation.
 
-The end goal of this application is to end up with large, anonymous UTXOs in your CashShuffle wallet. 
+The end goal of this application is to end up with large, anonymous UTXOs in your CashShuffle wallet.
+
+## Requirements
+* node __^10.15.1__
+* npm __^6.7.0__
+
+## Installation
+```bash
+git clone https://github.com/christroutner/shuffle-manager
+cd shuffle-manager
+npm install
+npm start
+```
--- shuffle-manager.js
+++ shuffle-manager.js
@@ -2,47 +2,50 @@
   A daemon for sending small BCH coins to CashFusion for consolidation.
 */
 
-'use strict'
+"use strict";
 
-const ElectronCash = require('./lib/ec')
-const electronCash = new ElectronCash()
+const ElectronCash = require("./lib/ec");
+const electronCash = new ElectronCash();
 
 // setInterval(function() {
 //   checkWallet()
 // }, 30000)
-checkWallet()
-
+checkWallet();
 
 async function checkWallet() {
   try {
-    const data = await electronCash.getSmallUtxos()
-    console.log(`data: ${JSON.stringify(data,null,2)}`)
+    const data = await electronCash.getSmallUtxos();
+    console.log(`data: ${JSON.stringify(data, null, 2)}`);
 
     // For each utxo found under the threshold, send it to the CashFusion wallet.
-    for(let i=0; i < data.length; i++) {
-    // for(let i=0; i < 1; i++) {
-      const thisUtxo = data[i]
+    for (let i = 0; i < data.length; i++) {
+      // for(let i=0; i < 1; i++) {
+      const thisUtxo = data[i];
 
-      const toAddr = await electronCash.getFusionAddr()
-      console.log(`fusion addr: ${toAddr}`)
+      const toAddr = await electronCash.getFusionAddr();
+      console.log(`fusion addr: ${toAddr}`);
 
-      const fromAddr = thisUtxo.address
-      console.log(`Sending UTXOs at this address: ${fromAddr}`)
+      const fromAddr = thisUtxo.address;
+      console.log(`Sending UTXOs at this address: ${fromAddr}`);
 
-      const hex = await electronCash.sendUtxo(toAddr, fromAddr)
-      // console.log(`hex: ${hex}`)
+      try {
+        const hex = await electronCash.sendUtxo(toAddr, fromAddr);
+        // console.log(`hex: ${hex}`)
 
-      const txid = await electronCash.broadcast(hex)
-      console.log(`Transaction broadcast with TXID: ${txid}`)
+        const txid = await electronCash.broadcast(hex);
+        console.log(`Transaction broadcast with TXID: ${txid}`);
 
-      console.log(`Sent utxo ${i}`)
-      console.log(' ')
+        console.log(`Sent utxo ${i}`);
+      } catch (err) {
+        console.log(`Error trying to send UTXO ${i} with address ${fromAddr}`);
+      }
 
-      await sleep(5000)
+      console.log(" ");
+
+      await sleep(5000);
     }
-
-  } catch(err) {
-    console.log(`Error in checkWallet(): `, err)
+  } catch (err) {
+    console.log(`Error in checkWallet(): `, err);
   }
 }
 
